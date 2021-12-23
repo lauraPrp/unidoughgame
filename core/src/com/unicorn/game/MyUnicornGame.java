14083e8cd7350434e1acc529fb7bb50416bf38ce
lauraPrp <laura.porpiglia@gmail.com>
2021-12-23 18:16:59 +0000

    score and winsound added

--- core/src/com/unicorn/game/MyUnicornGame.java
+++ core/src/com/unicorn/game/MyUnicornGame.java
@@ -7,129 +7,172 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
+
 import java.util.Iterator;
+
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
 public class MyUnicornGame extends ApplicationAdapter {
-	private Texture dropImage;
-	private Texture bucketImage;
-	private Sound dropSound;
-	//private Music rainMusic;
-	private SpriteBatch batch;
-	private OrthographicCamera camera;
-	private Rectangle bucket;
-	private Array<Rectangle> raindrops;
-	private long lastDropTime;
+    private Texture dropImage;
+    private Texture bucketImage;
+    private Texture bgImage;
+    private Sound dropSound;
+    private Sound winSound;
+    //private Music rainMusic;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Rectangle bucket;
+    private Array<Rectangle> raindrops;
+    private long lastDropTime;
+    private int score = 0;
+    private String scorePrint;
+    private BitmapFont scoreFont;
 
-	@Override
-	public void create() {
-		// load the images for the droplet and the bucket, 64x64 pixels each
-		dropImage = new Texture(Gdx.files.internal("doughnut.png"));
-		bucketImage = new Texture(Gdx.files.internal("unicorn.png"));
+    private String stringPoints(int point) {
+        return "" + point;
+    }
 
-		// load the drop sound effect and the rain background "music"
-		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-	//	rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+    private int intPoints(String pointStr) {
+        return Integer.parseInt(pointStr);
+    }
 
-		// start the playback of the background music immediately
-	//	rainMusic.setLooping(true);
-	//	rainMusic.play();
+    @Override
+    public void create() {
+        // load the images for the droplet and the bucket, 64x64 pixels each
+        bgImage = new Texture(Gdx.files.internal("bg.jpg"));
+        dropImage = new Texture(Gdx.files.internal("doughnut.png"));
+        bucketImage = new Texture(Gdx.files.internal("unicorn.png"));
 
-		// create the camera and the SpriteBatch
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-		batch = new SpriteBatch();
+        // load the drop sound effect and the rain background "music"
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        winSound = Gdx.audio.newSound(Gdx.files.internal("yay.wav"));
+        //	rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 
-		// create a Rectangle to logically represent the bucket
-		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
-		bucket.y = 20; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
-		bucket.width = 64;
-		bucket.height = 64;
+        // start the playback of the background music immediately
+        //	rainMusic.setLooping(true);
+        //	rainMusic.play();
 
-		// create the raindrops array and spawn the first raindrop
-		raindrops = new Array<Rectangle>();
-		spawnRaindrop();
-	}
+        // create the camera and the SpriteBatch
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        batch = new SpriteBatch();
 
-	private void spawnRaindrop() {
-		Rectangle raindrop = new Rectangle();
-		raindrop.x = MathUtils.random(0, 800-64);
-		raindrop.y = 480;
-		raindrop.width = 64;
-		raindrop.height = 64;
-		raindrops.add(raindrop);
-		lastDropTime = TimeUtils.nanoTime();
-	}
+        // create a Rectangle to logically represent the bucket
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
+        bucket.y = 20; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
+        bucket.width = 64;
+        bucket.height = 64;
+        // manage points
+        scorePrint = "Hello";
+        scoreFont = new BitmapFont(Gdx.files.internal("unicorngame.fnt"));
+        float aspectRatio = (float) Gdx.graphics.getHeight() /
+                (float) Gdx.graphics.getWidth();
+        scoreFont.getData().setScale(0.5f);
 
-	@Override
-	public void render() {
-		// clear the screen with a dark blue color. The
-		// arguments to clear are the red, green
-		// blue and alpha component in the range [0,1]
-		// of the color to be used to clear the screen.
-		ScreenUtils.clear(0, 0, 0.2f, 1);
+        // create the raindrops array and spawn the first raindrop
+        raindrops = new Array<Rectangle>();
+        spawnRaindrop();
 
-		// tell the camera to update its matrices.
-		camera.update();
+    }
 
-		// tell the SpriteBatch to render in the
-		// coordinate system specified by the camera.
-		batch.setProjectionMatrix(camera.combined);
+    private void spawnRaindrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800 - 64);
+        raindrop.y = 480;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
 
-		// begin a new batch and draw the bucket and
-		// all drops
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for(Rectangle raindrop: raindrops) {
-			batch.draw(dropImage, raindrop.x, raindrop.y);
-		}
-		batch.end();
+    @Override
+    public void render() {
+        // clear the screen with a dark blue color. The
+        // arguments to clear are the red, green
+        // blue and alpha component in the range [0,1]
+        // of the color to be used to clear the screen.
+        ScreenUtils.clear(0, 0, 0.2f, 1);
 
-		// process user input
-		if(Gdx.input.isTouched()) {
-			Vector3 touchPos = new Vector3();
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			camera.unproject(touchPos);
-			bucket.x = touchPos.x - 64 / 2;
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+        // tell the camera to update its matrices.
+        camera.update();
 
-		// make sure the bucket stays within the screen bounds
-		if(bucket.x < 0) bucket.x = 0;
-		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+        // tell the SpriteBatch to render in the
+        // coordinate system specified by the camera.
+        batch.setProjectionMatrix(camera.combined);
 
-		// check if we need to create a new raindrop
-		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+        // begin a new batch and draw the bucket and
+        // all drops
+        batch.begin();
+        batch.draw(bgImage, 0, 0);
+        scorePrint = stringPoints(score);
+        scoreFont.draw(batch, scorePrint, 50, 450);
+        batch.draw(bucketImage, bucket.x, bucket.y);
+        for (Rectangle raindrop : raindrops) {
+            batch.draw(dropImage, raindrop.x, raindrop.y);
+        }
 
-		// move the raindrops, remove any that are beneath the bottom edge of
-		// the screen or that hit the bucket. In the latter case we play back
-		// a sound effect as well.
-		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
-			Rectangle raindrop = iter.next();
-			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-			if(raindrop.y + 64 < 0) iter.remove();
-			if(raindrop.overlaps(bucket)) {
-				dropSound.play();
-				iter.remove();
-			}
-		}
-	}
+        batch.end();
 
-	@Override
-	public void dispose() {
-		// dispose of all the native resources
-		dropImage.dispose();
-		bucketImage.dispose();
-		dropSound.dispose();
-	//	rainMusic.dispose();
-		batch.dispose();
-	}
+        // process user input
+        if (Gdx.input.isTouched()) {
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            bucket.x = touchPos.x - 64 / 2;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+        // make sure the bucket stays within the screen bounds
+        if (bucket.x < 0) bucket.x = 0;
+        if (bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+        // check if we need to create a new raindrop
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+        // move the raindrops, remove any that are beneath the bottom edge of
+        // the screen or that hit the bucket. In the latter case we play back
+        // a sound effect as well.
+
+
+            for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+
+                Rectangle raindrop = iter.next();
+                raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+                if (raindrop.y + 64 < 0) iter.remove();
+                if (raindrop.overlaps(bucket)) {
+                    dropSound.play();
+                    score++;
+                    iter.remove();
+                }
+
+                if (score >= 10) {
+                    winSound.play();
+                }
+        }
+
+    }
+
+
+    @Override
+    public void dispose() {
+        // dispose of all the native resources
+        dropImage.dispose();
+        bucketImage.dispose();
+        dropSound.dispose();
+        winSound.dispose();
+        //	rainMusic.dispose();
+        batch.dispose();
+        scoreFont.dispose();
+    }
 }
